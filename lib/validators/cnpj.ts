6feef6d1292b85ebc530cec60b5a0d7310d43f7f
Benjamim <benjamimq015@gmail.com>
2021-02-25 18:48:15 -0400

    feat: ensure CnpjValidator returns false if a invalid CNPJ is provided

--- lib/validators/cnpj.ts
+++ lib/validators/cnpj.ts
@@ -12,11 +12,35 @@ class CNPJValidator {
     '99999999999999',
   ];
 
+  private checkDigits(cnpjNumbers: Array<string>) {
+    const reverse = cnpjNumbers.reverse();
+
+    let index = 2;
+    let sum = 0;
+
+    reverse.forEach(number => {
+      sum += Number(number) * index;
+      if (index === 9) index = 1;
+      index += 1;
+    });
+
+    return sum % 11 < 2 ? 0 : 11 - (sum % 11);
+  }
+
   public isValid(cnpj: string): boolean {
     const cnpjToValidate = cnpj.replace(/[^\d]+/g, '');
+
     if (cnpjToValidate.length !== 14) return false;
-    if (this.invalidCNPJs.includes(cnpj)) return false;
-    return true;
+    if (this.invalidCNPJs.includes(cnpjToValidate)) return false;
+
+    let cnpjNumbers = cnpjToValidate.substring(0, 12);
+
+    const cnpjWithoutCheckDigits = cnpjToValidate.substring(0, 12).split('');
+
+    cnpjNumbers += this.checkDigits(cnpjWithoutCheckDigits);
+    cnpjNumbers += this.checkDigits(cnpjNumbers.split(''));
+
+    return cnpjNumbers === cnpjToValidate;
   }
 }
 
