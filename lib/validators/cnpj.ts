3366995ba7227818faa14d4a6bc8b246a16ea0e0
Benjamim <benjamimq015@gmail.com>
2021-02-25 17:28:53 -0400

    feat: ensure CnpjValidator returns false if a known invalid CNPJ is provided

--- lib/validators/cnpj.ts
+++ lib/validators/cnpj.ts
@@ -1,7 +1,21 @@
 class CNPJValidator {
+  private invalidCNPJs = [
+    '00000000000000',
+    '11111111111111',
+    '22222222222222',
+    '33333333333333',
+    '44444444444444',
+    '55555555555555',
+    '66666666666666',
+    '77777777777777',
+    '88888888888888',
+    '99999999999999',
+  ];
+
   public isValid(cnpj: string): boolean {
     const cnpjToValidate = cnpj.replace(/[^\d]+/g, '');
     if (cnpjToValidate.length !== 14) return false;
+    if (this.invalidCNPJs.includes(cnpj)) return false;
     return true;
   }
 }
